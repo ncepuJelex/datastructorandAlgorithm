546c3eb95f86831707cd92727720157f33a02a54
ncepuJelex <1170366657@qq.com>
2017-11-30 22:10:14 +0800

    快速排序(in place形式)，不借外物，更省空间

--- src/com/jel/tech/learn/ch12/QuickSort.java
+++ src/com/jel/tech/learn/ch12/QuickSort.java
@@ -8,7 +8,11 @@ import com.jel.tech.learn.ch06.Queue;
 /**
  * 快速排序，和 合并排序一样，也有divide，conquer, concate result
  * 3个步骤，但不同的是，快速排序主要的工作量在递归前已经完成了，而
- * 我们知道合并排序主要的工作量是在每个递归中的合并结果过程中，可参看quick-sort.png图片理解
+ * 我们知道合并排序主要的工作量是在每个递归中的合并结果过程中，可参看quick-sort.png图片理解,
+ * Although quick-sort has very good performance on large data sets,
+ * it has rather high overhead on relatively small data sets,
+ * In practice, a simple algorithm like insertion- sort (Section 7.6)
+ * will execute faster when sorting such a short sequence.
  * @author jelex.xu
  * @date 2017年11月28日
  */
@@ -56,4 +60,69 @@ public class QuickSort {
 			S.enqueue(G.dequeue());
 		}
 	}
+
+	/**
+	 * An algorithm is in-place if it uses only a small amount of memory
+	 * in addition to that needed for the original input.
+	 * 上面的算法不是in place算法，因为还另外引入了G E L3个Queue
+	 * @param S
+	 * @param comp
+	 */
+	public static <K> void quickSortInplace(K[] S, Comparator<K> comp) {
+		quickSortInplace(S, comp, 0, S.length);
+	}
+	/*
+	 * 可参看quick-sort-in-place.png理解
+	 */
+	private static <K> void quickSortInplace(K[] s, Comparator<K> comp, int start, int end) {
+
+		if(start >= end) return;
+
+		int left = start;
+		int right = end-1;
+		//最后一个元素作为 pivot
+		K pivot = s[end];
+		K temp;
+		while(left <= right) {
+			/*
+			 * scan until reaching value equal or larger than pivot (or right marker)
+			 * 往右遍历，直到一个元素大于等于pivot（如果没有这样的元素，那直接遍历到最右边了）
+			 */
+			while(left<=right && comp.compare(s[left], pivot) < 0) left++;
+			/*
+			 * 和上面相反
+			 */
+			while(left<=right && comp.compare(s[right], pivot) > 0) right++;
+
+			if(left<=right) {
+				/*
+				 * swap
+				 */
+				temp = s[left];
+				s[left] = s[right];
+				s[right] = temp;
+				/*
+				 *缩短范围
+				 */
+				left++;
+				right--;
+			}
+		}
+		/*
+		 * 跳出了循环，此时left位置和pivot位置交换，让pivot归位，
+		 * left的位置是属于pivot的
+		 */
+		temp = s[left];
+		s[left] = s[end];
+		s[end] = temp;
+		/*
+		 * 递归处理
+		 */
+		quickSortInplace(s, comp, start, left-1);
+		quickSortInplace(s, comp, left+1, end);
+	}
+
+
+
+
 }
